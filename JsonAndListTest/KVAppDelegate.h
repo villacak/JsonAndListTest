f33ad42aa1cd52bd8ef92d1d6fc482cca6fdc0f0
Klaus Villaca <klausvillaca@gmail.com>
2014-04-22 22:56:04 +1000

    Initial Commit

---
+++ JsonAndListTest/KVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KVAppDelegate.h
+//  JsonAndListTest
+//
+//  Created by Klaus Villaca on 22/04/2014.
+//  Copyright (c) 2014 Own. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JsonAndListTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JsonAndListTest
+//
+//  Created by Klaus Villaca on 22/04/2014.
+//  Copyright (c) 2014 Own. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KVAppDelegate class]));
+    }
+}
